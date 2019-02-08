3d45658e28873484c7a9905b2f5d20c8aa5f233d
lhthien123 <hle6@pipeline.sbcc.edu>
2019-02-07 20:44:18 -0800

    commit

---
+++ src/main.cc
@@ -0,0 +1,20 @@
+//
+// Created by lhthi on 2/7/2019.
+//
+/**
+ * CS 140 Object-Oriented Programming in C++
+ * Assignment: EX01-HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [CHANGE THIS INFORMATION]
+ */
+
+#include <iostream>
+#include <cs140>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return EXIT_SUCCESS;
+}
